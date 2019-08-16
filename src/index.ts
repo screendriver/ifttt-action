40bce8b964d909e5093f230f8a262b75bd8c47fd
Christian Rackerseder <git@echooff.de>
2019-08-16 11:42:35 +0200

    🚫 Don't export function

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import core from '@actions/core';
 
-export function run() {
-  const event = core.getInput('event', { required: true });
-  const key = core.getInput('key', { required: true });
+const event = core.getInput('event', { required: true });
+const key = core.getInput('key', { required: true });
 
-  core.debug(`Event: ${event}`);
-  core.debug(`Key: ${key}`);
-}
+core.debug(`Event: ${event}`);
+core.debug(`Key: ${key}`);
