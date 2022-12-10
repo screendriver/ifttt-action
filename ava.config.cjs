a862f45575805c672b7a606f53b4ae9173852615
Christian Rackerseder <git@echooff.de>
2022-12-10 09:48:01 +0100

    chore: remove AVA configuration

--- ava.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    files: ['test/**/*.test.*'],
-    extensions: ['ts'],
-    require: ['ts-node/register/transpile-only'],
-    environmentVariables: {
-        TS_NODE_PROJECT: 'tsconfig.base.json',
-    },
-};
